b508b3d45e5aa156abc37df2f84c631851259a38
frank <frank315111@gmail.com>
2017-02-13 12:32:03 +0800

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,10 @@
 module.exports = {
-  /**
-   * Application configuration section
-   * http://pm2.keymetrics.io/docs/usage/application-declaration/
-   */
+
   apps : [
 
-    // First application
     {
-      name      : “Node_demo”,
-      script    : "./bin/www",
+      name : "node_demo",
+      script : "./bin/www",
       env: {
         COMMON_VARIABLE: "true"
       },
@@ -18,18 +14,15 @@ module.exports = {
     }
   ],
 
-  /**
-   * Deployment section
-   * http://pm2.keymetrics.io/docs/usage/deployment/
-   */
+
   deploy : {
     production : {
-      user : “root”,
-      host : “maijinta.cn”,
+      user : "root",
+      host : "115.159.190.30",
       ref  : "origin/master",
       repo : "git@github.com:Barry215/node_demo.git",
       path : "/home/root/www/production",
-      "post-deploy" : "npm install && pm2 startOrRestart ecosystem.json --env production"
+      "post-deploy" : "npm install && pm2 startOrRestart ecosystem.config.js --env production"
     }
   }
-}
+};
