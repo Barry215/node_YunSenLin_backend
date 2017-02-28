b7f04bc7bd46ab69888a7bfc9212778f5145bc3b
frank <frank315111@gmail.com>
2017-02-28 11:03:07 +0800

    增加了分析页面

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '云端森林服务器平台' });
+  res.render('index', { title: '云端森林分析平台' });
 });
 
 module.exports = router;
